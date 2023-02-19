22add13f0a3db313aac0e02fda9477aa76f6c98f
RaminIsko <ramsmagast@gmail.com>
2023-02-19 16:28:49 +0600

    first exercise + gitignore

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("ЗАДАЧА, проверяющая является ли число палиндромом");
+Console.WriteLine("");
+void Palindrom(int el){
+    if((el % 10 == el / 10000) && (el / 10 % 10 == el / 1000 % 10)){
+        Console.WriteLine($"Число {el} - Палиндром");
+    }else{
+        Console.WriteLine($"Число {el} - не Палиндром");
+    };
+}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if(number > 10000 && number < 100000){
+    Palindrom(number);
+}
