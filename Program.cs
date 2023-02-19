a86d5d13ea20a2218842fb827644473fbbb236de
RaminIsko <ramsmagast@gmail.com>
2023-02-19 16:59:00 +0600

    second exercise

--- Program.cs
+++ Program.cs
@@ -12,3 +12,10 @@ int number = int.Parse(Console.ReadLine()!);
 if(number > 10000 && number < 100000){
     Palindrom(number);
 }
+
+Console.WriteLine("ЗАДАЧА находящая расстояние между двумя точками в 3D пространстве");
+void Distance(double x1, double x2, double y1, double y2, double z1, double z2){
+    double S = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+    Console.WriteLine($"Расстояние между двумя точками в 3D пространстве: {S}");
+}
+Distance(1,2,3,4,5,6);
